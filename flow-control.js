f787d4d333cc26fd06db6a0813674773932f36e2
scruffy-gopher-2217 <laura001choat@gmail.com>
2020-01-10 21:16:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,16 +7,16 @@ function basicTeenager(age) {
 
 function teenager(age) {
   if(age > 12 && age < 20) {
-    return "You are a teenager";
+    return "You are a teenager!";
   } else {
     return "You are not a teenager";
   }
 }
 
 function ageChecker(age) {
-  if( age >= 13 || age <= 20 ) {
+  if( age >= 13 || age <= 19 ) {
     return "You are a teenager!";
-  } else if (age == 12 || age < 12) {
+  } else if (age <= 12) {
     return "You are a kid";
   } else {
     return "You are a grownup";
